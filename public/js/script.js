b3e2ec4e8f9477cc27b8137bdfeb778e7d649bba
Maxemilian Grönblom <maxemiliang@gmail.com>
2015-10-05 21:43:14 +0300

    Fixa lite nya sidor :D

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ function updateHeader() {
     var name = document.getElementById("name").value;
     var h1 = document.getElementById("h1");
 
-    h1.innerHTML = "Hello " + name;
+    h1.innerHTML = "Hello " + name + "!";
 }
